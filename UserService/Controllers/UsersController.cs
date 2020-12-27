19a757f3e580bdbf9336de8760fc45c2bd163dba
Łukasz Franiewski <lukifraniewski@gmail.com>
2020-12-27 18:41:45 +0100

    Add project files.

---
+++ UserService/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.ServiceBus;
+
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsersController : ControllerBase
+    {
+        public readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetAll()
+        {
+        return Ok(_context.Users.ToList());
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Add(User p)
+        {
+            _context.Users.Add(p);
+            _context.SaveChanges();
+
+            return Created("/api/users/", p);
+
+        }
+
+    }
+}
---
+++ UserService/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Model
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public string age { get; set; }
+
+        public string email_adress { get; set; }
+
+        public string password { get; set; }
+
+    }
+}
